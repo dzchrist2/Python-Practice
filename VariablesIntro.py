4d51e05f5d782b519dcdcde80ac6f8582e7b7f31
dzchrist2 <dzchrist@asu.edu>
2022-11-05 10:18:57 -0700

    Create VariablesIntro.py

---
+++ VariablesIntro.py
@@ -0,0 +1,49 @@
+# Create a variable named name and set it equal to a string of whatever you want to name the character
+
+
+# Create a variable named className and set it equal to the string "Druid"
+
+
+# Create a variable named healthPoints and set it equal to the integer 20
+
+
+# Add 15 to the variable healthPoints
+
+
+# Pring the new value of healthPoints
+
+
+# Create a variable name experiencePoints and set it equal to the float 10.5
+
+
+# Multiply experiencePoints by 5
+
+
+# Print the new value of experiencePoints
+
+
+# Create a variable named hasAbility and set it equal to the boolean false
+
+
+# This creates a Class called Character. When the _init_ function is called, an object with that information is created
+class Character:
+    def __init__(self, name, className, healthPoints, experiencePoints):
+        self.name = name
+        self.className = className
+        self.healthPoints = healthPoints
+        self.experiencePoints = experiencePoints
+
+
+# Create a Character using the above class definition and the variables you defined
+# Type your variables within the parenthesis to give the function the information it needs
+myCharacter = Character()
+
+# Print your characters name using myCharacter.name to access it
+
+# Add 10 to your characters experiencePoints using myCharacter.exeriencePoints to access it
+
+
+# Print your character's new experiencePoints
+
+
+# Create a second Character, filling in the informatio however you want
